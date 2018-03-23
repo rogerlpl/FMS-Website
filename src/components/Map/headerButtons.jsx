e7a1b6eae05acf076a52d4117430870c0742a9b1
Luis Rogelio Lopez Ulloa <lr.lopezulloa@outlook.com>
2018-03-23 14:41:58 -0400

    Se agregaron los botones para geocercas y para rutas

--- src/components/Map/headerButtons.jsx
+++ src/components/Map/headerButtons.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 import {
-  IconButton,
-  Hidden
+    IconButton,
+    Hidden
 } from "material-ui";
 
-import { AddLocation} from "material-ui-icons";
+import { Directions, AddLocation } from "material-ui-icons";
 
-
+const divStyle = {
+    display: 'inline'
+}
 const headerButtons = (props) => (
-    // boton para las geocercas
-    <IconButton
-    color="inherit"
-    aria-label="AddLocation"
-    className={props.classes.buttonLink}
-   >
-     <AddLocation className={props.classes.links}/>
-     <Hidden mdUp>
-       <p className={props.classes.linkText}>Geocercas</p>
-       </Hidden>
-     </ IconButton>
+<div style={divStyle}>
+        {/* boton para las rutas */}
+        <IconButton
+            color="inherit"
+            aria-label="AddLocation"
+            className={props.classes.buttonLink}
+        >
+            <Directions className={props.classes.links} />
+            <Hidden mdUp>
+                <p className={props.classes.linkText}>Rutas</p>
+            </Hidden>
+        </ IconButton>
+        {/*boton para las geocercas */}
+        <IconButton
+            color="inherit"
+            aria-label="AddLocation"
+            className={props.classes.buttonLink}
+        >
+            <AddLocation className={props.classes.links} />
+            <Hidden mdUp>
+                <p className={props.classes.linkText}>Geocercas</p>
+            </Hidden>
+        </ IconButton>
 
+</div>
 )
 
 export default headerButtons
